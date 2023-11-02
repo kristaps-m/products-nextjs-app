5190d1b4b757f8596c71d75bcc9f1bfc2fc82a9f
Kristaps-M <kristapsmitins@gmail.com>
2023-11-02 23:45:08 +0200

    Data using ProductDetails.tsx is displayed in the webpage.

--- pages/ProductDetails.tsx
+++ pages/ProductDetails.tsx
@@ -5,11 +5,27 @@ import { Product, Root } from "../src/app/models/product";
 import agent from "../src/app/api/agent";
 import { useParams } from "next/navigation";
 
+const productDetailsStyle = {
+  width: "50%",
+  margin: "0 auto",
+  padding: "20px",
+  border: "1px solid #ccc",
+  borderRadius: "5px",
+};
+
+const backLinkStyle = {
+  display: "block",
+  marginTop: "20px",
+  textDecoration: "none",
+  color: "blue",
+};
+
 const ProductDetails = () => {
   const router = useRouter();
-  // const { id } = useParams<{ id: number }>();
   const { productId } = router.query; // Access the product ID from the route parameter
-  const [product, setProduct] = useState<Root | null>(null);
+  const [product, setProduct] = useState<Product | null>(null); // Root
+  // const [product, setProduct] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     if (productId) {
@@ -19,34 +35,45 @@ const ProductDetails = () => {
     }
   }, [productId]);
 
-  // useEffect(() => {
-  //   if (productId) {
-  //     agent.Catalog.details(Number(productId))
-  //       .then((data) => {
-  //         // Check if data has a 'products' property and extract the first product
-  //         if (data && data.products && data.products.length > 0) {
-  //           setProduct(data.products[0]);
-  //         } else {
-  //           console.error("No product data found.");
-  //         }
-  //       })
-  //       .catch((error) => console.error("Error fetching product data:", error));
-  //   }
-  // }, [productId]);
-
-  console.log(product, "this is the one product");
-  //console.log(product.products, "this is product.products");
+  useEffect(() => {
+    if (productId) {
+      agent.Catalog.details(Number(productId))
+        .then((data) => {
+          const idToFind = Number(productId);
+          const foundProduct = data.products.find(
+            (p: Product) => p.id === idToFind
+          );
+          if (foundProduct) {
+            setProduct(foundProduct);
+          } else {
+            console.error("No product data found.");
+          }
+        })
+        .catch((error) => console.error("Error fetching product data:", error))
+        .finally(() => {
+          setLoading(false); // Set loading state to false when data is fetched
+        });
+    }
+  }, [productId]);
 
   return (
     <div>
-      <h1>Product Details for Product ID: {productId}</h1>
-      {product?.products.map((p) => (
-        <div key={p.id}>
-          <p>{p.category}</p>
-        </div>
-      ))}
-      {/* <p>Category: {product?.products}</p> */}
-      {/* Display product details based on the productId */}
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : product ? (
+          <div style={productDetailsStyle}>
+            <h3>{product.name}</h3>
+            <p>
+              Price: {product.price} {product.currency}
+            </p>
+            <p>Category: {product.category}</p>
+            <p>Description: {product.description}</p>
+          </div>
+        ) : (
+          <p>No product found.</p>
+        )}
+      </div>
     </div>
   );
 };
--- src/features/product/ProductCard.tsx
+++ src/features/product/ProductCard.tsx
@@ -1,14 +1,3 @@
-// import { LoadingButton } from "@mui/lab";
-// import {
-//   Avatar,
-//   Button,
-//   Card,
-//   CardActions,
-//   CardContent,
-//   CardHeader,
-//   CardMedia,
-//   Typography,
-// } from "@mui/material";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import agent from "../../app/api/agent";
@@ -50,47 +39,5 @@ export default function ProductCard({ product }: Props) {
         <button className="button-style">View</button>
       </a>
     </div>
-    // <Card>
-    //   <CardHeader
-    //     avatar={
-    //       <Avatar sx={{ bgcolor: "secondary.main" }}>
-    //         {product.name.charAt(0).toUpperCase()}
-    //       </Avatar>
-    //     }
-    //     title={product.name}
-    //     titleTypographyProps={{
-    //       sx: { fontWeight: "bold", color: "primary.main" },
-    //     }}
-    //   />
-    //   <CardMedia
-    //     sx={{
-    //       height: 140,
-    //       backgroundSize: "contain",
-    //       bgcolor: "primary.light",
-    //     }}
-    //     image={product.pictureUrl}
-    //     title={product.name}
-    //   />
-    //   <CardContent>
-    //     <Typography gutterBottom color="secondary" variant="h5">
-    //       {currencyFormat(product.price)}
-    //     </Typography>
-    //     <Typography variant="body2" color="text.secondary">
-    //       {product.brand} / {product.type}
-    //     </Typography>
-    //   </CardContent>
-    //   <CardActions>
-    //     <LoadingButton
-    //       loading={loading}
-    //       onClick={() => handleAddItem(product.id)}
-    //       size="small"
-    //     >
-    //       Add to cart
-    //     </LoadingButton>
-    //     <Button component={Link} to={`catalog/${product.id}`} size="small">
-    //       View
-    //     </Button>
-    //   </CardActions>
-    // </Card>
   );
 }
